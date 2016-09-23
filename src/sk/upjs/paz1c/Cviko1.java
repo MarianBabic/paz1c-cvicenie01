a92a51e558646980e0ae041504b6dcbc583df914
student <student@158.197.35.85>
2016-09-23 07:56:07 +0200

    sysout 5x

--- src/sk/upjs/paz1c/Cviko1.java
+++ src/sk/upjs/paz1c/Cviko1.java
@@ -12,8 +12,10 @@ package sk.upjs.paz1c;
 public class Cviko1 {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello world" + i + "!");
+        }
     }
-    
+
 }
